abb6f69d42def7e80027fab157d71e3a419b0efa
davideochoaa <davideochoaa@gmail.com>
2023-06-13 01:59:08 +0200

    ✅ Feat: Imported react and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://davidochoadev.github.io',
-   base: '/intro-component-with-sign-up-form',
+  site: 'https://davidochoadev.github.io',
+  base: '/intro-component-with-sign-up-form',
+  integrations: [react(), tailwind()]
 });
---
+++ src/components/Component.tsx
@@ -0,0 +1,7 @@
+const Comp = () => {
+   return (
+      <h2>This is a tsx component</h2>
+   )
+}
+
+export default Comp
